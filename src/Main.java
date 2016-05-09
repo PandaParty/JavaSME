5bf6ba0e5641a9d483e47a4f80ef981bd068783a
Anton Josefsson <gusjosanl@student.gu.se>
2016-05-09 21:37:11 +0200

    Added SMEStandardIO and examples using it

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import sme.Level;
 import sme.SMEFile;
 import sme.SMEIO;
+import sme.SMEStandardIO;
 
 import java.io.File;
 import java.util.HashMap;
@@ -8,8 +9,8 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args){
         HashMap<SMEIO, String> map = new HashMap<>();
-        example2(Level.LOW, map);
-        example2(Level.HIGH, map);
+        example4(Level.LOW, map);
+        example4(Level.HIGH, map);
     }
 
     private static void example1(Level level, HashMap<SMEIO, String> map){
@@ -30,5 +31,26 @@ public class Main {
         String highContents = highFile.input(level, map);
         System.out.println(highContents);
         lowFile.output(highContents, level);
+    }
+
+
+    private static void example3(Level level, HashMap<SMEIO, String> map){
+        System.out.println("Running at level: " + level.toString());
+        SMEStandardIO lowLevelStandardIO = new SMEStandardIO(Level.LOW);
+        SMEFile highFile = new SMEFile(new File("highFile.txt"), Level.HIGH);
+        lowLevelStandardIO.output("Please input your what to put in file", level);
+        String input = lowLevelStandardIO.input(level, map);
+        highFile.output(input, level);
+    }
+
+    private static void example4(Level level, HashMap<SMEIO, String> map){
+        System.out.println("Running at level: " + level.toString());
+        SMEStandardIO highLevelStandardIO = new SMEStandardIO(Level.HIGH);
+        SMEFile lowFile = new SMEFile(new File("lowFile.txt"), Level.LOW);
+        highLevelStandardIO.output("Please input your password", level);
+        String password = highLevelStandardIO.input(level, map);
+        if(password == "master"){
+            lowFile.output("illegal flow was executed", level);
+        }
     }
 }
---
+++ src/sme/SMEStandardIO.java
@@ -0,0 +1,49 @@
+package sme;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SMEStandardIO implements SMEIO {
+    private Level level;
+
+    public SMEStandardIO(Level _level){
+        level = _level;
+    }
+
+    public void output(String s, Level runLevel){
+        if(level == runLevel){
+            System.out.println(s);
+        }
+    }
+
+    public String input(Level runLevel, HashMap<SMEIO, String> map){
+        if(runLevel == level){
+            Scanner scan = new Scanner(System.in);
+            String inputString = scan.nextLine();
+            map.put(this, inputString);
+            return inputString;
+        }
+        else if(runLevel.higherThan(level)){
+            if(map.containsKey(this)){
+                return map.get(this);
+            }
+            else{
+                return "Shit's on fire, yo";
+            }
+        }
+        return "Can not read input of higher level";
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(!(o instanceof SMEStandardIO)) return super.equals(o);
+
+        SMEStandardIO so = (SMEStandardIO)o;
+        return level == so.level;
+    }
+
+    @Override
+    public int hashCode(){
+        return level.hashCode();
+    }
+}
